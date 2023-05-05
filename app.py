41e2e0a6ede22d52f577f5531ec470171384ea47
Shivam Negi <shivam2612002@gmail.com>
2023-05-06 04:17:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import subprocess
+from flask import request,Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("form.html")
+
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    exercise_type = request.form.get('exercise_type')
+    command = f"python main.py -t {exercise_type}"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return result.stdout
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
